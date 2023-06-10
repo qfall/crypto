34b4a619b01342cfc38916db39d171f813776692
Jan Niklas Siemer <niklas.siemer@gmail.com>
2023-06-10 22:13:24 +0200

    Implement dual regev PubParams generation + tests

--- src/construction/pk_encryption/dual_regev.rs
+++ src/construction/pk_encryption/dual_regev.rs
@@ -83,6 +83,105 @@ impl DualRegev {
         Ok(Self { n, m, q, r, alpha })
     }
 
+    /// Generates a new [`DualRegev`] instance, i.e. a new set of suitable public parameters,
+    /// given the security parameter `n`.
+    ///
+    /// Parameters:
+    /// - `n`: specifies the security parameter and number of rows
+    ///   of the uniform at random instantiated matrix `A`
+    ///
+    /// Returns a correct and secure [`DualRegev`] PK encryption instance or
+    /// a [`MathError`] if the given `n <= 1`.
+    ///
+    /// # Example
+    /// ```
+    /// use qfall_crypto::construction::pk_encryption::DualRegev;
+    ///
+    /// let dual_regev = DualRegev::new_from_n(2).unwrap();
+    /// ```
+    ///
+    /// # Errors and Failures
+    /// - Returns a [`MathError`] of type [`InvalidIntegerInput`](MathError::InvalidIntegerInput)
+    /// if `n <= 1`.
+    pub fn new_from_n(n: impl Into<Z>) -> Result<Self, MathError> {
+        let n = n.into();
+        if n <= Z::ONE {
+            return Err(MathError::InvalidIntegerInput(String::from(
+                "n must be chosen bigger than 1.",
+            )));
+        }
+
+        let mut m: Z;
+        let mut q: Z;
+        let mut r: Q;
+        let mut alpha: Q;
+        (m, q, r, alpha) = Self::gen_new_public_parameters(&n);
+        while Self::check_params(&n, &m, &q, &r, &alpha).is_err() {
+            (m, q, r, alpha) = Self::gen_new_public_parameters(&n);
+        }
+
+        let q = Modulus::try_from_z(&q).unwrap();
+
+        Ok(Self { n, m, q, r, alpha })
+    }
+
+    /// Generates new public parameters, which must not be secure or correct
+    /// depending on the random choice of `q`. At least every fifth execution
+    /// of this function should output a valid set of public parameters,
+    /// ensuring a secure and correct PK encryption scheme.
+    ///
+    /// Parameters:
+    /// - `n`: specifies the security parameter and number of rows
+    ///   of the uniform at random instantiated matrix `A`
+    ///
+    /// Returns a set of public parameters `(m, q, r, alpha)` chosen according to
+    /// the provided `n`.
+    ///
+    /// # Example
+    /// ```compile_fail
+    /// use qfall_crypto::construction::pk_encryption::DualRegev;
+    /// use qfall_math::integer::Z;
+    /// let n = Z::from(2);
+    ///
+    /// let (m, q, r, alpha) = DualRegev::gen_new_public_parameters(&n);
+    /// ```
+    fn gen_new_public_parameters(n: &Z) -> (Z, Z, Q, Q) {
+        let n_i64 = i64::try_from(n).unwrap();
+        // these powers are chosen according to experience s.t. at least every
+        // fifth generation of public parameters outputs a valid pair
+        let power = match n_i64 {
+            2 => 9,
+            3 => 8,
+            4..=5 => 7,
+            6..=8 => 6,
+            9..=12 => 5,
+            13..=30 => 4,
+            31..=5000 => 3,
+            _ => 2,
+        };
+
+        // generate prime q in [n^power / 2, n^power]
+        // TODO: Replace by q = Z::sample_prime_uniform once implemented
+        let upper_bound: Z = n.pow(power).unwrap();
+        let lower_bound = upper_bound.div_ceil(&Z::from(2));
+        let mut q = Z::sample_uniform(&lower_bound, &upper_bound).unwrap();
+        while !q.is_prime() {
+            q = Z::sample_uniform(&lower_bound, &upper_bound).unwrap();
+        }
+
+        // choose m = 2 (n+1) lg q
+        let m = (Z::from(2) * (n + Z::ONE) * q.log(&10).unwrap()).round();
+
+        // choose r = log m
+        let r = m.log(&2).unwrap();
+
+        // alpha = 1/(sqrt(m) * log^2 m)
+        // TODO: remove ceil, when log is applicable to Qs
+        let alpha = 1 / m.sqrt() * (m.log(&2).unwrap()).ceil().log(&2).unwrap();
+
+        (m, q, r, alpha)
+    }
+
     /// Checks a provided set of public parameters according to their validity
     /// regarding security and completeness according to
     /// Lemma 8.2 and 8.4 of [\[1\]](<index.html#:~:text=[1]>).
@@ -308,6 +407,7 @@ impl PKEncryption for DualRegev {
 #[cfg(test)]
 mod test_pp_generation {
     use super::DualRegev;
+    use super::Z;
 
     /// Checks whether `new` works properly for correct and secure parameter choices
     #[test]
@@ -328,6 +428,18 @@ mod test_pp_generation {
         assert!(DualRegev::new(1, 16, 401, 4, 20).is_err());
     }
 
+    /// Checks whether `new_from_n` works properly for different choices of n
+    #[test]
+    fn suitable_security_params() {
+        let n_choices = [
+            2, 3, 4, 5, 6, 8, 9, 12, 13, 30, 21, 50, 100, 250, 500, 1000, 2500, 5000, 5001, 10000,
+        ];
+
+        for n in n_choices {
+            assert!(DualRegev::new_from_n(n).is_ok());
+        }
+    }
+
     /// Checks whether the [`Default`] parameter choice is suitable
     #[test]
     fn default_suitable() {
@@ -336,4 +448,99 @@ mod test_pp_generation {
         assert!(DualRegev::check_params(&dr.n, &dr.m, &Z::from(&dr.q), &dr.r, &dr.alpha).is_ok());
     }
 
+    /// Checks whether the generated public parameters from `new_from_n` are
+    /// valid choices according to security and correctness of the scheme
+    #[test]
+    fn choice_valid() {
+        let n_choices = [2, 3, 5, 8, 10, 14, 25, 50, 125, 300, 600, 1200, 4000, 6000];
+
+        for n in n_choices {
+            let dr = DualRegev::new_from_n(n).unwrap();
+            assert!(
+                DualRegev::check_params(&dr.n, &dr.m, &Z::from(&dr.q), &dr.r, &dr.alpha).is_ok()
+            );
+        }
+    }
+
+    /// Ensures that `new_from_n` is available for types implementing [`Into<Z>`]
+    #[test]
+    fn availability() {
+        let _ = DualRegev::new_from_n(2u8);
+        let _ = DualRegev::new_from_n(2u16);
+        let _ = DualRegev::new_from_n(2u32);
+        let _ = DualRegev::new_from_n(2u64);
+        let _ = DualRegev::new_from_n(2i8);
+        let _ = DualRegev::new_from_n(2i16);
+        let _ = DualRegev::new_from_n(2i32);
+        let _ = DualRegev::new_from_n(2i64);
+        let _ = DualRegev::new_from_n(Z::from(2));
+        let _ = DualRegev::new_from_n(&Z::from(2));
+    }
+
+    /// Checks whether `new_from_n` returns an error for invalid input n
+    #[test]
+    fn invalid_n() {
+        assert!(DualRegev::new_from_n(1).is_err());
+        assert!(DualRegev::new_from_n(0).is_err());
+        assert!(DualRegev::new_from_n(-1).is_err());
+    }
+}
+
+#[cfg(test)]
+mod test_dual_regev {
+    use super::DualRegev;
+    use crate::construction::pk_encryption::PKEncryption;
+    use qfall_math::integer::Z;
+
+    /// Checks whether the full-cycle of gen, enc, dec works properly
+    /// for message 0 and small n
+    #[test]
+    fn cycle_zero_small_n() {
+        let msg = Z::ZERO;
+        let dr = DualRegev::new_from_n(5).unwrap();
+
+        let (pk, sk) = dr.gen();
+        let cipher = dr.enc(&pk, &msg);
+        let m = dr.dec(&sk, &cipher);
+        assert_eq!(msg, m);
+    }
+
+    /// Checks whether the full-cycle of gen, enc, dec works properly
+    /// for message 1 and small n
+    #[test]
+    fn cycle_one_small_n() {
+        let msg = Z::ONE;
+        let dr = DualRegev::new_from_n(5).unwrap();
+
+        let (pk, sk) = dr.gen();
+        let cipher = dr.enc(&pk, &msg);
+        let m = dr.dec(&sk, &cipher);
+        assert_eq!(msg, m);
+    }
+
+    /// Checks whether the full-cycle of gen, enc, dec works properly
+    /// for message 0 and larger n
+    #[test]
+    fn cycle_zero_large_n() {
+        let msg = Z::ZERO;
+        let dr = DualRegev::new_from_n(50).unwrap();
+
+        let (pk, sk) = dr.gen();
+        let cipher = dr.enc(&pk, &msg);
+        let m = dr.dec(&sk, &cipher);
+        assert_eq!(msg, m);
+    }
+
+    /// Checks whether the full-cycle of gen, enc, dec works properly
+    /// for message 1 and larger n
+    #[test]
+    fn cycle_one_large_n() {
+        let msg = Z::ONE;
+        let dr = DualRegev::new_from_n(50).unwrap();
+
+        let (pk, sk) = dr.gen();
+        let cipher = dr.enc(&pk, &msg);
+        let m = dr.dec(&sk, &cipher);
+        assert_eq!(msg, m);
+    }
 }
