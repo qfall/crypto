bcdb62c6857d25111b69a9a081abcedcdcdb1296
Marvin Beckmann <marvin17@mail.uni-paderborn.de>
2023-06-21 11:01:06 +0200

    add references + doc-comments to psfs

--- src/sample/distribution/psf.rs
+++ src/sample/distribution/psf.rs
@@ -11,10 +11,21 @@
 
 pub mod gpv;
 
+/// This trait should be implemented by all constructions that are
+/// actual implementations of a [`PSF`].
+/// A formal definition for these PSFs can be found in
+/// [\[1\]](<../index.html#:~:text=[1]>)
 pub trait PSF<A, Trapdoor, Domain, Range> {
+    /// Samples a parity-check matrix and a trapdoor for that matrix
     fn trap_gen(&self) -> (A, Trapdoor);
+    /// Samples an element in the domain according to a specified distribution
     fn samp_d(&self) -> Domain;
+    /// Samples an element `e` in the domain according to a specified distribution
+    /// conditioned on `fa(a, e) = u`
     fn samp_p(&self, a: &A, r: &Trapdoor, u: &Range) -> Domain;
+    /// Implements the efficiently computable function `fa`
+    /// which is uniquely classified by `a`
     fn fa(&self, a: &A, sigma: &Domain) -> Range;
+    /// Checks whether an element is in the correct domain (and not just the correct type)
     fn check_dn(&self, sigma: &Domain) -> bool;
 }
--- src/sample/distribution/psf/gpv.rs
+++ src/sample/distribution/psf/gpv.rs
@@ -6,7 +6,8 @@
 // the terms of the Mozilla Public License Version 2.0 as published by the
 // Mozilla Foundation. See <https://mozilla.org/en-US/MPL/2.0/>.
 
-//! Implements a GPV PSF according to: ... using G-Trapdoors (TODO ref etc)
+//! Implements a GPV PSF according to [\[1\]](<../index.html#:~:text=[1]>)
+//! using G-Trapdoors to generate a short basis and corresponding trapdoor.
 
 use super::PSF;
 use crate::sample::g_trapdoor::{
@@ -14,52 +15,207 @@ use crate::sample::g_trapdoor::{
     short_basis_classical::gen_short_basis_for_trapdoor,
 };
 use qfall_math::{
-    integer::MatZ,
+    integer::{MatZ, Z},
     integer_mod_q::MatZq,
     rational::{MatQ, Q},
-    traits::Pow,
+    traits::{GetNumRows, Pow},
 };
 
+/// A lattice-based implementation of a [`PSF`] according to
+/// [\[1\]](<../index.html#:~:text=[1]>) using
+/// G-Trapdoors where D_n = \{e \in \Z^m | |e| \leq s \sqrt{m}\}
+/// and R_n = Z_q^n.
+///
+/// Attributes
+/// - `gp`: describes the gadget parameters with which the G-Trapdoor is generated
+/// - `s`: the standard deviation with which is sampled
+///
+/// # Examples
+/// ```
+/// use qfall_crypto::sample::distribution::psf::gpv::PSFGPV;
+/// use qfall_crypto::sample::g_trapdoor::gadget_parameters::GadgetParameters;
+/// use qfall_math::rational::Q;
+/// use qfall_math::integer_mod_q::Modulus;
+/// use crate::qfall_crypto::sample::distribution::psf::PSF;
+///
+/// let modulus = Modulus::from(64);
+/// let psf = PSFGPV {
+///     gp: GadgetParameters::init_default(8, &modulus),
+///     s: Q::from(12),
+/// };
+///
+/// let (a, r) = psf.trap_gen();
+/// let domain_sample = psf.samp_d();
+/// let range_fa = psf.fa(&a, &domain_sample);
+/// let preimage = psf.samp_p(&a, &r, &range_fa);
+///
+/// // TODO: include it, once parameters are revised
+/// // assert!(psf.check_dn(&preimage));
+/// ```
 pub struct PSFGPV {
-    pub pp: GadgetParameters,
+    pub gp: GadgetParameters,
     pub s: Q,
 }
 
 impl PSF<MatZq, MatZ, MatZ, MatZq> for PSFGPV {
+    /// Computes a G-Trapdoor according to the [`GadgetParameters`]
+    /// defined in the struct.
+    ///
+    /// # Examples
+    /// ```
+    /// use qfall_crypto::sample::distribution::psf::gpv::PSFGPV;
+    /// use qfall_crypto::sample::g_trapdoor::gadget_parameters::GadgetParameters;
+    /// use qfall_math::rational::Q;
+    /// use qfall_math::integer_mod_q::Modulus;
+    /// use crate::qfall_crypto::sample::distribution::psf::PSF;
+    ///
+    /// let modulus = Modulus::from(64);
+    /// let psf = PSFGPV {
+    ///     gp: GadgetParameters::init_default(8, &modulus),
+    ///     s: Q::from(12),
+    /// };
+    ///
+    /// let (a, r) = psf.trap_gen();
+    /// ```
     fn trap_gen(&self) -> (MatZq, MatZ) {
-        let a_bar = MatZq::sample_uniform(&self.pp.n, &self.pp.m_bar, &self.pp.q);
+        let a_bar = MatZq::sample_uniform(&self.gp.n, &self.gp.m_bar, &self.gp.q);
 
-        let tag = MatZq::identity(&self.pp.n, &self.pp.n, &self.pp.q);
+        let tag = MatZq::identity(&self.gp.n, &self.gp.n, &self.gp.q);
 
-        gen_trapdoor(&self.pp, &a_bar, &tag).unwrap()
+        gen_trapdoor(&self.gp, &a_bar, &tag).unwrap()
     }
 
+    /// Samples in the domain using SampleD with the standard basis and center `0`.
+    ///
+    /// # Examples
+    /// ```
+    /// use qfall_crypto::sample::distribution::psf::gpv::PSFGPV;
+    /// use qfall_crypto::sample::g_trapdoor::gadget_parameters::GadgetParameters;
+    /// use qfall_math::rational::Q;
+    /// use qfall_math::integer_mod_q::Modulus;
+    /// use crate::qfall_crypto::sample::distribution::psf::PSF;
+    ///
+    /// let modulus = Modulus::from(64);
+    /// let psf = PSFGPV {
+    ///     gp: GadgetParameters::init_default(8, &modulus),
+    ///     s: Q::from(12),
+    /// };
+    /// let (a, r) = psf.trap_gen();
+    ///
+    /// let domain_sample = psf.samp_d();
+    /// ```
     fn samp_d(&self) -> MatZ {
-        let m = &self.pp.n * &self.pp.k + &self.pp.m_bar;
+        let m = &self.gp.n * &self.gp.k + &self.gp.m_bar;
         let m = i64::try_from(&m).unwrap();
-        MatZ::sample_d_common(m, &self.pp.n, &self.s).unwrap()
+        let s = &self.s * (Q::from(2) * Q::PI).sqrt();
+        MatZ::sample_d_common(m, &self.gp.n, &s).unwrap()
     }
 
+    /// Samples an `e` in the domain using SampleD with a short basis that is generated
+    /// from the G-Trapdoor from the conditioned conditioned
+    /// discrete gaussian with `f_a(a,e) = u` for a provided syndrome `u`.
+    ///
+    /// Parameters:
+    /// - `a`: The parity-check matrix
+    /// - `r`: The G-Trapdoor for `a`
+    /// - `u`: The syndrome from the range
+    ///
+    /// Returns a sample `e` from the domain on the conditioned discrete
+    /// gaussian distribution `fa(a,e) = u`.
+    ///
+    /// # Examples
+    /// ```
+    /// use qfall_crypto::sample::distribution::psf::gpv::PSFGPV;
+    /// use qfall_crypto::sample::g_trapdoor::gadget_parameters::GadgetParameters;
+    /// use qfall_math::rational::Q;
+    /// use qfall_math::integer_mod_q::Modulus;
+    /// use crate::qfall_crypto::sample::distribution::psf::PSF;
+    ///
+    /// let modulus = Modulus::from(64);
+    /// let psf = PSFGPV {
+    ///     gp: GadgetParameters::init_default(8, &modulus),
+    ///     s: Q::from(12),
+    /// };
+    /// let (a, r) = psf.trap_gen();
+    /// let domain_sample = psf.samp_d();
+    /// let range_fa = psf.fa(&a, &domain_sample);
+    ///
+    /// let preimage = psf.samp_p(&a, &r, &range_fa);
+    /// ```
     fn samp_p(&self, a: &MatZq, r: &MatZ, u: &MatZq) -> MatZ {
-        let tag = MatZq::identity(&self.pp.n, &self.pp.n, &self.pp.q);
+        let tag = MatZq::identity(&self.gp.n, &self.gp.n, &self.gp.q);
 
-        let short_basis = gen_short_basis_for_trapdoor(&self.pp, &tag, a, r);
+        let short_basis = gen_short_basis_for_trapdoor(&self.gp, &tag, a, r);
 
         let sol: MatZ = (&a.solve_gaussian_elimination(u).unwrap()).into();
 
         let center = MatQ::from(&(-1 * &sol));
         let s = &self.s * (Q::from(2) * Q::PI).sqrt();
-        let signature: MatZ = MatZ::sample_d(&short_basis, &self.pp.n, &center, &s).unwrap() + sol;
+        let signature: MatZ = MatZ::sample_d(&short_basis, &self.gp.n, &center, &s).unwrap() + sol;
 
         signature
     }
 
+    /// Implements the efficiently computable function `fa` which here corresponds to
+    /// `a*value`
+    ///
+    /// Parameters:
+    /// - `a`: The parity-check matrix
+    /// - `value`: A Value from the domain
+    ///
+    /// Returns `a*value`
+    ///
+    /// # Examples
+    /// ```
+    /// use qfall_crypto::sample::distribution::psf::gpv::PSFGPV;
+    /// use qfall_crypto::sample::g_trapdoor::gadget_parameters::GadgetParameters;
+    /// use qfall_math::rational::Q;
+    /// use qfall_math::integer_mod_q::Modulus;
+    /// use crate::qfall_crypto::sample::distribution::psf::PSF;
+    ///
+    /// let modulus = Modulus::from(64);
+    /// let psf = PSFGPV {
+    ///     gp: GadgetParameters::init_default(8, &modulus),
+    ///     s: Q::from(12),
+    /// };
+    /// let (a, r) = psf.trap_gen();
+    /// let domain_sample = psf.samp_d();
+    /// let range_fa = psf.fa(&a, &domain_sample);
+    /// ```
     fn fa(&self, a: &MatZq, value: &MatZ) -> MatZq {
         a * value
     }
 
+    /// Checks whether a value `sigma` is in D_n = \{e \in \Z^m | |e| \leq s \sqrt{m}\}.
+    ///
+    /// Attributes
+    /// - `sigma`: The value for which is checked, if it is in the domain
+    ///
+    /// Returns true, if `sigma` is in D_n.
+    ///
+    /// # Examples
+    /// ```
+    /// use qfall_crypto::sample::distribution::psf::gpv::PSFGPV;
+    /// use qfall_crypto::sample::g_trapdoor::gadget_parameters::GadgetParameters;
+    /// use qfall_math::rational::Q;
+    /// use qfall_math::integer_mod_q::Modulus;
+    /// use crate::qfall_crypto::sample::distribution::psf::PSF;
+    /// use qfall_math::integer::MatZ;
+    ///
+    /// let modulus = Modulus::from(64);
+    /// let psf = PSFGPV {
+    ///     gp: GadgetParameters::init_default(8, &modulus),
+    ///     s: Q::from(12),
+    /// };
+    /// let (a, r) = psf.trap_gen();
+    ///
+    /// let vector = MatZ::new(8, 1);
+    ///
+    /// assert!(psf.check_dn(&vector));
+    /// ```
     fn check_dn(&self, sigma: &MatZ) -> bool {
-        let m = &self.pp.n * &self.pp.k + &self.pp.m_bar;
-        Q::from(&sigma.norm_eucl_sqrd().unwrap()) <= self.s.pow(2).unwrap() * m
+        let m = &self.gp.n * &self.gp.k + &self.gp.m_bar;
+        Q::from(&sigma.norm_eucl_sqrd().unwrap()) <= self.s.pow(2).unwrap() * &m
+            && self.gp.n == Z::from(sigma.get_num_rows())
     }
 }
