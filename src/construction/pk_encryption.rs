56a412e75509f242a2a1e8df74e5af5c003413f9
Jan Niklas Siemer <niklas.siemer@gmail.com>
2023-07-29 22:56:34 +0200

    Implement generic multi-bit encryption for PKEs

--- src/construction/pk_encryption.rs
+++ src/construction/pk_encryption.rs
@@ -39,14 +39,100 @@ pub use lpr::LPR;
 pub use regev::Regev;
 pub use regev_discrete_gauss::RegevWithDiscreteGaussianRegularity;
 
-use qfall_math::integer::Z;
+use qfall_math::{integer::Z, integer_mod_q::Zq};
 
+/// This trait should be implemented by every public key encryption scheme.
+/// It offers a simple interface to use and implement PKEs.
 pub trait PKEncryption {
     type PublicKey;
     type SecretKey;
     type Cipher;
 
+    /// Generates a public key pair `(pk, sk)` suitable for the specific scheme.
+    ///
+    /// Returns a tuple `(pk, sk)` consisting of [`Self::PublicKey`] and [`Self::SecretKey`].
     fn gen(&self) -> (Self::PublicKey, Self::SecretKey);
+
+    /// Encrypts the provided `message` using the public key `pk`.
+    ///
+    /// Parameters:
+    /// - `pk`: specifies the public key used for encryption
+    /// - `message`: specifies the message to be encrypted
+    ///
+    /// Returns the encryption of `message` as a [`Self::Cipher`] instance.
     fn enc(&self, pk: &Self::PublicKey, message: impl Into<Z>) -> Self::Cipher;
+
+    /// Decrypts the provided `cipher` using the secret key `sk`.
+    ///
+    /// Parameters:
+    /// - `sk`: specifies the secret key used for decryption
+    /// - `cipher`: specifies the ciphertext to be decrypted
+    ///
+    /// Returns the decryption of `cipher` as a [`Z`] instance.
     fn dec(&self, sk: &Self::SecretKey, cipher: &Self::Cipher) -> Z;
+}
+
+/// This trait generically implements multi-bit encryption
+/// for any scheme implementing the [`PKEncryption`] trait.
+///
+/// It splits the given ciphertext up into its bits and
+/// stores the individual encrypted bits as a vector of ciphertexts.
+pub trait GenericMultiBitEncryption: PKEncryption {
+    /// Encrypts multiple bits by appending several encryptions of single bits.
+    /// The order of single ciphers is `[c0, c1, ..., cn]`, where `c0` is the least significant bit.
+    /// Negative values are not allowed. Hence, the absolute value is being encrypted.
+    ///
+    /// Parameters:
+    /// - `pk`: specifies the public key
+    /// - `message`: specifies the message that should be encryted
+    ///
+    /// Returns a cipher of type [`Vec`] containing [`PKEncryption::Cipher`].
+    fn enc_multiple_bits(&self, pk: &Self::PublicKey, message: impl Into<Z>) -> Vec<Self::Cipher> {
+        let mut message: Z = message.into().abs();
+
+        let mut power_of_two = Z::from(2);
+        let mut cipher = vec![];
+        for _i in 0..message.bits() {
+            // get message mod 2^_i
+            let c_i = Zq::from((&message, &power_of_two)).get_value();
+
+            // if message mod 2^_i == 0 -> encrypt zero and append to ciphertext
+            // otherwise encrypt one and append to ciphertext
+            if c_i == Z::ZERO {
+                cipher.push(self.enc(pk, Z::ZERO));
+            } else {
+                cipher.push(self.enc(pk, Z::ONE));
+            }
+
+            // update values for next loop
+            // remove encrypted bit from message
+            message = message - c_i;
+            // compute next power of two
+            power_of_two = power_of_two * 2;
+        }
+
+        cipher
+    }
+
+    /// Decrypts a multiple bit ciphertext.
+    ///
+    /// Parameters:
+    /// - `sk`: specifies the secret key used for decryption
+    /// - `cipher`: specifies a slice of ciphers containing several [`PKEncryption::Cipher`] instances
+    /// to be decrypted
+    ///
+    /// Returns the decryption of `cipher` as a [`Z`] instance.
+    fn dec_multiple_bits(&self, sk: &Self::SecretKey, cipher: &[Self::Cipher]) -> Z {
+        let mut message = Z::ZERO;
+        let mut power_of_two = Z::ONE;
+
+        for item in cipher {
+            let msg_i = self.dec(sk, item);
+            message = message + msg_i * &power_of_two;
+
+            power_of_two = power_of_two * 2;
+        }
+
+        message
+    }
 }
