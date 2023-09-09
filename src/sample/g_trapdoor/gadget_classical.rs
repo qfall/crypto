44f6ccd7fd59775e278fbfd0777c999593e81c38
Jan Niklas Siemer <niklas.siemer@gmail.com>
2023-09-09 11:36:55 +0200

    Apply function signature update to merge

--- src/sample/g_trapdoor/gadget_classical.rs
+++ src/sample/g_trapdoor/gadget_classical.rs
@@ -410,9 +410,6 @@ mod test_find_solution_gadget {
 
         let sol = find_solution_gadget_mat(&value, &k, &base);
 
-        assert_eq!(
-            MatZ::from(&value),
-            gen_gadget_mat(3, &k, &base).unwrap() * sol
-        )
+        assert_eq!(MatZ::from(&value), gen_gadget_mat(3, &k, &base) * sol)
     }
 }
